4bdc8554d87cdc04db3e100127c86809dc74e253
Axel Bellec <axel.bellec@outlook.fr>
2017-09-14 16:16:50 +0200

    define and add a clock process for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn app:app
+clock: python chronos/clock.py
---
+++ chronos/clock.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from chronos.cli import main
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=5)
+def timed_job():
+    print('This job is run every five minutes.')
+    main()
+    
+
+if __name__ == '__main__':
+    sched.start()
