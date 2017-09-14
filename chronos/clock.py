4729bee4c86373ec4206427562ffd29a148cda3f
Axel Bellec <axel.bellec@outlook.fr>
2017-09-14 20:09:35 +0200

    update hreoku clock file

--- chronos/clock.py
+++ chronos/clock.py
@@ -1,19 +1,20 @@
 # coding: utf-8
 
+""" Custom clock script for Heroku. """
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from chronos.cli import cli
 from chronos.tracing import log_factory
 
 log = log_factory(__name__)
-sched = BlockingScheduler()
 
-
-@sched.scheduled_job('interval', minutes=5)
 def timed_job():
     log.info('this job is run every five minutes')
     cli()
     
 
 if __name__ == '__main__':
+    sched = BlockingScheduler()
+    sched.add_job(timed_job, 'cron', id='run_every_5_min', minute='*/5')
     sched.start()
